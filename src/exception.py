835f49f5c3a1ca5f8e09193b905f977c2ede95f3
Uddipan Goswami <sohan.goswami@gmail.com>
2024-03-30 19:42:53 +0530

    DataIngestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -9,7 +10,7 @@ def error_message_detail(error,error_detail:sys):
     
 class customexception(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
